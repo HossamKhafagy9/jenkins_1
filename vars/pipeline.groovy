e434007ce9f7c696d074a7b6d07bb027bfe13a43
HossamKhafagy9 <68228871+HossamKhafagy9@users.noreply.github.com>
2023-06-26 16:44:53 +0300

    Update pipeline.groovy

--- vars/pipeline.groovy
+++ vars/pipeline.groovy
@@ -1,36 +1,36 @@
-pipeline {
-     agent any
-    parameters {
-        booleanParam(name:'project', defaultValue: true, description:'this paramater help you to know project name')
-        choice(name: 'namespace', choices:['dev','prod','stage'], description: '' ) 
-    }
-
-    stages {
-        stage('check') {
-            steps {
-                echo "checking your code"
-                echo "${params.namespace}"
-               
-            }
+def call() {
+    pipeline {
+        agent any
+        parameters {
+            booleanParam(name: 'project', defaultValue: true, description: 'This parameter helps you to know the project name')
+            choice(name: 'namespace', choices: ['dev', 'prod', 'stage'], description: 'Choose the namespace')
         }
 
-        stage('test') {
-            when {
-                expression{
-                    params.project == true 
+        stages {
+            stage('check') {
+                steps {
+                    echo "Checking your code"
+                    echo "Namespace: ${params.namespace}"
                 }
             }
-            steps {
-                echo "testing your app" 
+
+            stage('test') {
+                when {
+                    expression {
+                        params.project == true
+                    }
+                }
+                steps {
+                    echo "Testing your app"
+                }
+            }
+
+            stage('deployment') {
+                steps {
+                    echo "Your code is being deployed right now"
+                    echo "This build number: $BUILD_NUMBER"
+                }
             }
         }
-        
-        stage('deployment') {  
-            steps {
-                echo "your code is deployed right now"
-                echo "this build number $BUILD_NUMBER"
-            }
-        }    
     }
-
 }
