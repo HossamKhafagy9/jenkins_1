eb58d8d3b8d0d7ccf3d7198d385ece5af354df83
HossamKhafagy9 <68228871+HossamKhafagy9@users.noreply.github.com>
2023-06-26 17:09:31 +0300

    Update pipeline.groovy

--- vars/pipeline.groovy
+++ vars/pipeline.groovy
@@ -1,14 +1,33 @@
 def call() {
-    stage('Test') {
-        steps {
-            echo "Testing your app"
+    pipeline {
+        agent any
+        parameters {
+            booleanParam(name:'project', defaultValue: true, description:'this paramater help you to know project name')
+            choice(name: 'namespace', choices:['dev','prod','stage'], description: '' )
         }
-    }
-
-    stage('Deploy') {
-        steps {
-            echo "Your code is being deployed right now"
-            echo "This build number: $BUILD_NUMBER"
+        stages {
+            stage('check') {
+                steps {
+                    echo "checking your code"
+                    echo "${params.namespace}"
+                }
+            }
+            stage('test') {
+                when {
+                    expression{
+                        params.project == true
+                    }
+                }
+                steps {
+                    echo "testing your app"
+                }
+            }
+            stage('deployment') {
+                steps {
+                    echo "your code is deployed right now"
+                    echo "this build number $BUILD_NUMBER"
+                }
+            }
         }
     }
 }
