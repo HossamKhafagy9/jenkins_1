0bc65dbddbba60af5db91de746bc67c18d0acf5c
HossamKhafagy9 <68228871+HossamKhafagy9@users.noreply.github.com>
2023-06-26 16:13:24 +0300

    Update full_pipeline.groovy

--- var/full_pipeline.groovy
+++ var/full_pipeline.groovy
@@ -1,39 +1,36 @@
-def call(){
-    pipeline {
-         agent any
-        parameters {
-            booleanParam(name:'project', defaultValue: true, description:'this paramater help you to know project name')
-            choice(name: 'namespace', choices:['dev','prod','stage'], description: '' ) 
+pipeline {
+     agent any
+    parameters {
+        booleanParam(name:'project', defaultValue: true, description:'this paramater help you to know project name')
+        choice(name: 'namespace', choices:['dev','prod','stage'], description: '' ) 
+    }
+
+    stages {
+        stage('check') {
+            steps {
+                echo "checking your code"
+                echo "${params.namespace}"
+               
+            }
         }
 
-        stages {
-            stage('check') {
-                steps {
-                    echo "checking your code"
-                    echo "${params.namespace}"
-
+        stage('test') {
+            when {
+                expression{
+                    params.project == true 
                 }
             }
-
-            stage('test') {
-                when {
-                    expression{
-                        params.project == true 
-                    }
-                }
-                steps {
-                    echo "testing your app" 
-                }
+            steps {
+                echo "testing your app" 
             }
+        }
         
-            stage('deployment') {  
-               steps {
-                    echo "your code is deployed right now"
-                    echo "this build number $BUILD_NUMBER"
-                }
-            }    
-        }
-
+        stage('deployment') {  
+            steps {
+                echo "your code is deployed right now"
+                echo "this build number $BUILD_NUMBER"
+            }
+        }    
     }
 
 }
